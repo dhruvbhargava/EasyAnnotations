d825bd4f1c1fd80d073ca90f634c2a7276d5e6aa
dhruvbhargava <dhruv.muj@gmail.com>
2020-04-25 09:39:16 +0530

    v 0.2.1 minor refactor

--- AnnotationScreen.py
+++ AnnotationScreen.py
@@ -8,8 +8,6 @@ from PyQt5.QtWidgets import QWidget as widget
 import PyQt5.QtCore as QtCore
 import BoundingBox as BB
 from BoundingBox import TaggedObject as obj
-from SelectorLayover import SelectorOverlay as SO
-# from Generators import Generator  as gen
 
 class labelDialog(widgets.QDialog):
      def __init__(self, *args, **kwargs):
